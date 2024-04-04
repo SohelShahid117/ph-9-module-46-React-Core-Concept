cc3c51fda00642361273f8964a042bcd1073ea9c
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-05 00:43:56 +0600

    complete the code on 46-1 Handle Form With OnSubmit And Access Field Data

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -4,12 +4,23 @@ const SimpleForm = () => {
     const handleSubmit = (e) =>{
         e.preventDefault();
         console.log(e)
+        console.log(e.target)
+        console.log(e.target.name)
+        console.log(e.target.name.value)
+        console.log(e.target.email.value)
+        console.log(e.target.password.value)
         // console.log('form submitted')
     }
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input type="text" />
+                {/* <input type="text" /> */}
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" />
+                <br />
+                {/* <input type="password" name="password" /> */}
+                <input type="password" name="password" id="" />
                 <br />
                 {/* <button>Submit</button> */}
                 <input type="submit" value="Submittt" />
