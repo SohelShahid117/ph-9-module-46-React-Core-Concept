212170a37e9ac3840be6f3ea94a0e4f0352d144e
SohelShahid117 <sohelshahid09@gmail.com>
2024-04-10 16:32:13 +0600

    46-8 How To Use Context API To Avoid Prop Drilling done

--- src/Components/GrandPa/GrandPa.jsx
+++ src/Components/GrandPa/GrandPa.jsx
@@ -1,7 +1,11 @@
 //46-6 Create A List Of Components To Create Connection
 //46-7 Prop Drilling To Pass Data To A Many Level Components
+//46-8 How To Use Context API To Avoid Prop Drilling
 
 //https://react.dev/learn/passing-data-deeply-with-context
+//https://react.dev/learn/passing-data-deeply-with-context
+//https://react.dev/reference/react/useContext
+//https://react.dev/learn/reusing-logic-with-custom-hooks
 
 import React from 'react';
 import Dad from '../Dad/Dad';
